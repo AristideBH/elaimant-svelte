418ef49ed7db466d4451e694c79762167302af80
Aristide <aristide.bruneau@gmail.com>
2023-11-10 12:50:53 +0100

    move modules files to lib/package

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // Reexport your entry components here
-import Elaimant from "./Elaimant.svelte";
-import type { ElaimantOptions } from "./elaimant.js";
+import Elaimant from "./package/Elaimant.svelte";
+import type { ElaimantOptions } from "./package/elaimant.js";
 
 export default Elaimant
 export type { ElaimantOptions }
